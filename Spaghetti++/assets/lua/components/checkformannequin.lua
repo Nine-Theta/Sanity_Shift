519613622e735061b7db3876bec064922b76ec78
David <davidheetebrij@gmail.com>
2019-03-14 14:18:17 +0100

    deathscene implemented, experience may not match expectations

--- Spaghetti++/assets/lua/components/checkformannequin.lua
+++ Spaghetti++/assets/lua/components/checkformannequin.lua
@@ -1,33 +1,13 @@
 --parent is component's parent gameObject. It supports functions 
 
-function start()
-	parent:setName("Enemy")
-end
-speed = math.random(150,350)
-function update() --currently disabled
---	print("Updated lua component") 
-end
-
-function fixedupdate()
-	--print(parent.getPos())
-	local x,y = parent:getPos()
-	parent:setPos(x - speed * time:fixedDelta(),y)
-	if x < -400 then
-		parent:delete()
-	end
-end
-
-function onmessage(message)
-	print(parent:getName() .. " received a message: ")
-	print(message)
-end
-
-function oncollision(other)
+function oncollisionenter(other)
 	local name = other:getName()
-	if name == "Bullet" then
-		local score = gameObject.find("score")
-		score:sendMessage(25)
-		audio.play("PlayerDeathSound.wav")
-		parent:delete()
+	if name == "Mannequin" then
+		print("You Died")
+		gameObject.deleteAll()
+		obj = gameObject.new()
+		obj:setName("Loader")
+		obj:addComponent("lua", "sceneloader.lua")
+		obj:sendMessage("../deathscene.lua")
 	end
 end
---
+++ Spaghetti++/assets/lua/components/endscenethings.lua
@@ -0,0 +1,13 @@
+--parent is component's parent gameObject. It supports functions 
+
+function start()
+	parent:playSound()
+	--audio.play("frolic.wav")
+end
+
+function fixedupdate()
+end
+	
+function ondestroy()
+
+end
