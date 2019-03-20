95d3f6db826309406a99ca3b0b9969350c1ac542
David <davidheetebrij@gmail.com>
2019-03-20 17:08:03 +0100

    startmenu buttons no longer get deleted or created upon select/deselect

--- Spaghetti++/assets/lua/components/resumegamebutton.lua
+++ Spaghetti++/assets/lua/components/resumegamebutton.lua
@@ -23,13 +23,13 @@ end
 function Selected()
 	parent:removeComponent("mesh")
 	parent:addComponent("mesh","ResumeGameTestSelect.obj","brown.dds","white_s.png","flat_n.png")
-	--print("ResumeGameButton Selected")
+	print("ResumeGameButton Selected")
 end
 
 function Deselected()
 	parent:removeComponent("mesh")
 	parent:addComponent("mesh","ResumeGameTest.obj","lightGray.dds","white_s.png","flat_n.png")
-	--print("ResumeGameButton Deselected")
+	print("ResumeGameButton Deselected")
 end
 
 function Invoke()
