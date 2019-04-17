9befd3dcca3230169757ca820e474b71065d2864
Devin Braune <devin@devinbraune.de>
2019-04-17 17:20:32 +0200

    Finished animation lua code, needs to be tweaked once the assets are in

--- Spaghetti++/assets/lua/components/animMove.lua
+++ Spaghetti++/assets/lua/components/animMove.lua
@@ -1,18 +1,14 @@
 --parent is component's parent gameObject. It supports functions 
 
 function start()
-	player = parent:getParent():getParent()
-	player:removeComponent("lua")
-	player:removeComponent("lua")
-	waypoints = gameObject.find("AnimationPoints"):getChildren()
+	waypoints = gameObject.find("AnimationPointsMove"):getChildren()
 	wpCount = #waypoints
 	target = waypoints[0]
-	lookTarget = gameObject.find("AnimLookTarget")
 end
 
 waypoint = 0
 
-force = 400
+force = 0
 
 function length(fx,fy,fz)
 	return math.sqrt(math.pow(fx,2) + math.pow(fy,2) + math.pow(fz, 2))
@@ -23,33 +19,17 @@ function normalize(fx,fy,fz)
 	return fx / length,fy / length,fz / length
 end
 
-function updateView()
-	local rx,ry,rz = parent:forward()
-	rotX = math.atan2(rx,rz)*180/3.14
-	local tx,ty,tz = lookTarget:getWorldPos()
-	local px,py,pz = parent:getWorldPos()
-	targetRot = math.atan2(tx - px, tz - pz)*180/3.14
-	d = (rotX - targetRot)--/steps
-end
-
 function fixedupdate()
 	local px,py,pz = target:getWorldPos()
 	tx,ty,tz = parent:getWorldPos()
 	px,py,pz = normalize(px - tx,0,pz - tz)
 	--print("Waypoint target: " .. waypoint)
-	player:addForce(px * force,py * force,pz * force)
-	
-	if d ~= nil then
-		d = d * 0.99
-		parent:setWorldRotation(0,1,0,targetRot + d)
-	end
-	updateView()
+	parent:addForce(px * force,py * force,pz * force)
 end
 
 function ontriggerenter(col)
 	print("triggered")
 	if waypoint == wpCount then
-		lookTarget:sendMessage(1600)
 		force = 0
 		return
 	end
@@ -65,6 +45,10 @@ function ontriggerenter(col)
 	if force == nil then
 		force = 400
 	end
+end
+
+function onmessage(msg)
+	force = tonumber(msg)
 end
 
 function ondestroy()
