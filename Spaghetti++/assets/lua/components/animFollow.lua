3582a9b0b7ca8960b504ba832ee6b8af301a85ad
Devin Braune <devin@devinbraune.de>
2019-04-17 15:45:14 +0200

    Added simple player waypoint follower, no rotation involved yet

--- Spaghetti++/assets/lua/components/animFollow.lua
+++ Spaghetti++/assets/lua/components/animFollow.lua
@@ -1,24 +1,47 @@
 --parent is component's parent gameObject. It supports functions 
 
 function start()
+	player = parent:getParent():getParent()
+	player:removeComponent("lua")
+	player:removeComponent("lua")
+	waypoints = gameObject.find("AnimationPoints"):getChildren()
+	wpCount = #waypoints
+	target = waypoints[0]
 end
 
-rotation = 0
-verticalSensitivity = 10 --default: 10
-upperLimit = 80
-lowerLimit = -80
+waypoint = 0
 
-following = false
-enabled = true
-function update()
-	
+force = 400
+
+function length(fx,fy,fz)
+	return math.sqrt(math.pow(fx,2) + math.pow(fy,2) + math.pow(fz, 2))
 end
-	
+
+function normalize(fx,fy,fz)
+	local length = length(fx,fy,fz)
+	return fx / length,fy / length,fz / length
+end
+
 function fixedupdate()
-	print("following anim")
+	local px,py,pz = target:getWorldPos()
+	tx,ty,tz = parent:getWorldPos()
+	px,py,pz = normalize(px - tx,0,pz - tz)
+	--print("Waypoint target: " .. waypoint)
+	player:addForce(px * force,py * force,pz * force)
 end
 
-function onmessage(msg)
+function ontriggerenter(col)
+	print("triggered")
+	if waypoint == wpCount then
+		return
+	end
+	print(col)
+	print(target)
+	if col == target then
+		waypoint = waypoint + 1
+		target = waypoints[waypoint]
+		print("CHANGING WAYPOINT!!!")
+	end
 end
 
 function ondestroy()
