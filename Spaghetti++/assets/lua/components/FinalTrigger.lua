7efca0275949f396717fea936f28cecc227c4144
Devin Braune <devin@devinbraune.de>
2019-04-25 14:37:01 +0200

    Added outside wall and final door mannequin

--- Spaghetti++/assets/lua/components/FinalTrigger.lua
+++ Spaghetti++/assets/lua/components/FinalTrigger.lua
@@ -2,14 +2,23 @@
 
 function start()
 	finalDoor = gameObject.find("FinalDoor")
+	mannequin = parent:getChildren()[0]
+	mannequin:setActive(false)
 end
 
 function ontriggerenter(other)
 	local name = other:getName()
-	print("Player collided with " .. name)
 	if name == "AnimLookTarget" then
 		print("END SEQUENCE!")
 		print(finalDoor)
 		finalDoor:sendMessage("shut")
+	end
+end
+
+function ontriggerexit(other)
+	local name = other:getName()
+	if name == "Player" then
+		print("END SEQUENCE!")
+		mannequin:setActive(true)
 	end
 end
